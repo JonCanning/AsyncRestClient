6deb9c917a69b516fe013be0a6c7cc8f7315ee44
Jon Canning <jon.canning@gmail.com>
2014-02-10 20:59:59 +0000

    Removed unnecessary methods in IHttpClient

--- src/AsyncRestClient/IAsyncClient.cs
+++ src/AsyncRestClient/IAsyncClient.cs
@@ -16,12 +16,9 @@ namespace AsyncRestClient
         Task<TResponse> Get<TResponse>(string requestUri) where TResponse : class;
         Task<TResponse> Post<TRequest, TResponse>(TRequest request, string requestUri) where TRequest : class where TResponse : class;
         Task<TResponse> Put<TRequest, TResponse>(TRequest request, string requestUri) where TRequest : class where TResponse : class;
-
         Task<TResponse> Send<TRequest, TResponse>(TRequest request, HttpMethod httpMethod, string requestUri) where TRequest : class
             where TResponse : class;
-
         Task<TResponse> Send<TResponse>(HttpMethod httpMethod, string requestUri, HttpContent content = null) where TResponse : class;
-
         Task<TResponse> Post<TRequest, TResponse>(TRequest request, string requestUri, string fileName, Stream stream) where TRequest : class
             where TResponse : class;
     }
--- src/AsyncRestClient/IHttpClient.cs
+++ src/AsyncRestClient/IHttpClient.cs
@@ -10,40 +10,7 @@ namespace AsyncRestClient
     public interface IHttpClient
     {
         HttpRequestHeaders DefaultRequestHeaders { get; }
-        Uri BaseAddress { get; set; }
-        TimeSpan Timeout { get; set; }
-        long MaxResponseContentBufferSize { get; set; }
-        Task<string> GetStringAsync(string requestUri);
-        Task<string> GetStringAsync(Uri requestUri);
-        Task<byte[]> GetByteArrayAsync(string requestUri);
-        Task<byte[]> GetByteArrayAsync(Uri requestUri);
-        Task<Stream> GetStreamAsync(string requestUri);
-        Task<Stream> GetStreamAsync(Uri requestUri);
-        Task<HttpResponseMessage> GetAsync(string requestUri);
-        Task<HttpResponseMessage> GetAsync(Uri requestUri);
-        Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption);
-        Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption);
-        Task<HttpResponseMessage> GetAsync(string requestUri, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> GetAsync(Uri requestUri, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content);
-        Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content);
-        Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content);
-        Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content);
-        Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> DeleteAsync(string requestUri);
-        Task<HttpResponseMessage> DeleteAsync(Uri requestUri);
-        Task<HttpResponseMessage> DeleteAsync(string requestUri, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken);
         Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
-        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
-        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption);
-        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken);
-        void CancelPendingRequests();
         void Dispose();
     }
 }
